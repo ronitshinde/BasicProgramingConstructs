050598a5e7d2d95852c97da695b12840981536a2
ronitshinde <rony.shinde753@gmail.com>
2023-04-24 22:20:22 +0530

    [Ronit] Add : UC1-Check equality of two integers

---
+++ ProgramingConstructs/ProgramingConstructs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingConstructs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Equality of two integers");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    SelectionAndRepetation constructs = new SelectionAndRepetation();
+                    constructs.EqualityOfTwoIntegers();
+                    Console.ReadKey();
+                    break;
+            }
+           
+        }
+    }
+}
---
+++ ProgramingConstructs/ProgramingConstructs/SelectionAndRepetation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingConstructs
+{
+    public class SelectionAndRepetation
+    {
+        public void EqualityOfTwoIntegers()
+        {
+            /*UC1-Write a C# Sharp program to accept two integers and check whether
+            they are equal or not.*/
+            Console.WriteLine("Enter the first integer");
+            int firstInteger = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second integer");
+            int secondInteger = Convert.ToInt32(Console.ReadLine());
+            if(firstInteger.Equals(secondInteger))
+                Console.WriteLine("Both integers are equal");
+            else
+                Console.WriteLine("Both integers are unequal");
+        }
+    }
+}
