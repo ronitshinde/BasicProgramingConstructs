e43300b173d2f16fd93167e3cc094ead9379295a
ronitshinde <rony.shinde753@gmail.com>
2023-05-09 20:58:35 +0530

    [Ronit] Add : UC4-Code to check the maximum of three numbers

---
+++ ProgramingConstructs/ProgramingConstructs/LargestNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramingConstructs
+{
+    public class LargestNumber
+    {
+        //1. Write a C# Sharp program to find the largest of three numbers.
+        int greaterNum = 0;
+        public int OfThreeNumbers(int digitOne, int digitTwo, int digitThree)
+        {
+            if (digitOne > digitTwo)
+            {
+                if (digitOne > digitThree)
+                    greaterNum = digitOne;
+                else 
+                    greaterNum = digitThree;
+            }
+            else if (digitTwo > digitThree)
+            {
+                greaterNum = digitTwo;
+            }
+            else
+                greaterNum = digitThree;
+            return greaterNum;
+        }
+    }
+}
--- ProgramingConstructs/ProgramingConstructs/Program.cs
+++ ProgramingConstructs/ProgramingConstructs/Program.cs
@@ -10,7 +10,7 @@ namespace ProgramingConstructs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1.Equality of two integers\n2.Check even or odd\n3.Check eligibility to vote");
+            Console.WriteLine("1.Equality of two integers\n2.Check even or odd\n3.Check eligibility to vote\n4.Largest number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -28,9 +28,19 @@ namespace ProgramingConstructs
                     int age = Convert.ToInt32(Console.ReadLine());
                     vote.Voting(age);
                     break;
+                case 4:
+                    LargestNumber number = new LargestNumber();
+                    Console.WriteLine("Enter the three numbers : ");
+                    Console.Write("1 : ");
+                    int numOne = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2 : ");
+                    int numTwo = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("3 : ");
+                    int numThree = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Largest of three numbers is : " + number.OfThreeNumbers(numOne, numTwo, numThree));
+                    break;
             }
             Console.ReadKey();
-
         }
     }
 }
